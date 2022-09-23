8891752a668bd5c6f8fa6ed309b2a75c472bfcf8
neslişah ebral <nestay2001@gmail.com>
2022-09-23 09:03:21 +0300

    ornek proje eklendi

---
+++ src/Navi.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Navi companent</h2>
+      </div>
+    )
+  }
+}
+
